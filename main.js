879d0461cee24e3fdc2e9e2cd5c7ef9214cc8457
Thandei <67441515+Thandei@users.noreply.github.com>
2021-04-16 01:39:23 +0300

    changes made

--- main.js
+++ main.js
@@ -29,12 +29,15 @@ if(!açık){
   document.querySelector(".nav-open").style.display="flex";
   document.querySelector(".nav-open").style.right=0;
   document.querySelector("body").style.overflow="hidden";
+  document.querySelector(".backtoTop").style.display="none";
   açık=true;
   return;
 }else{
-  document.querySelector(".nav-open").style.right=-100+"%";
+  document.querySelector(".nav-open").style.display="none";
+   
   document.querySelector("body").style.overflowY="visible";
   document.querySelector("body").style.overflowX="hidden";
+  document.querySelector(".backtoTop").style.display="block";
   açık=false;
   return;
 }
